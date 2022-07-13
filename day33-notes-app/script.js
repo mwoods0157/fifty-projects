99b3ee5d66c28b24b31bda4b24d6a80f10563929
Matthew <matthew.woods0157@gmail.com>
2022-07-13 10:38:13 -0400

    Update local storage js feature

--- day33-notes-app/script.js
+++ day33-notes-app/script.js
@@ -1,5 +1,11 @@
 const addBtn = document.getElementById('add');
 
+const notes = JSON.parse(localStorage.getItem('notes'));
+
+if(notes) {
+    notes.forEach(note => addNewNote(note));
+}
+
 addBtn.addEventListener('click', () => addNewNote());
 
 function addNewNote(text = '') {
@@ -25,6 +31,8 @@ function addNewNote(text = '') {
 
     deleteBtn.addEventListener('click', () => {
         note.remove();
+
+        updateLS();
     })
 
     editBtn.addEventListener('click', () => {
@@ -36,7 +44,19 @@ function addNewNote(text = '') {
         const { value } = e.target;
 
         main.innerHTML = marked(value);
+
+        updateLS();
     })
 
     document.body.appendChild(note);
+}
+
+function updateLS() {
+    const notesText = document.querySelectorAll('textarea');
+
+    const notes = [];
+
+    notesText.forEach(note => notes.push(note.value));
+
+    localStorage.setItem('notes', JSON.stringify(notes));
 }
