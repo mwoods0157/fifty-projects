015c53a013ed582300be353d254da496abad7965
Matthew <matthew.woods0157@gmail.com>
2022-07-18 11:29:10 -0400

    add/remove color feature added

--- day36-hoverboard/script.js
+++ day36-hoverboard/script.js
@@ -14,10 +14,13 @@ for(let i = 0; i < squares; i++) {
 
 function setColor(element) {
     const color = getRandomColor();
+    element.style.background = color;
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
 }
 
 function removeColor(element) {
-
+    element.style.background = '#1d1d1d';
+    element.style.boxShadow = `0 0 2px #000`;
 }
 
 function getRandomColor() {
