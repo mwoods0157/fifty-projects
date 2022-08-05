daa79ccd6ab68c08fc4ada53ad5b5d344ab6f4a8
Matthew <matthew.woods0157@gmail.com>
2022-08-05 10:33:43 -0400

    btn event listener added

---
+++ day43-feedback-ui-design/script.js
@@ -0,0 +1,16 @@
+const ratings = document.querySelectorAll('rating');
+const sendBtn = document.querySelector('#send');
+const panel = document.querySelector('#panel');
+
+panel.addEventListener('click', (e) => {
+    if(e.target.parentNode.classList.contains('rating')) {
+        removeActive();
+        e.target.parentNode.classList.add('active');
+    }
+})
+
+function removeActive() {
+    for(let i = 0; i < ratings.length; i++) {
+        
+    }
+}
