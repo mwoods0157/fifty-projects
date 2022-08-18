1512bd5ee4219e673a7c0281d345c971ed5e75ae
Matthew <matthew.woods0157@gmail.com>
2022-08-18 15:51:12 -0400

    Js dom variables

--- day47-testimonial-box-switcher/script.js
+++ day47-testimonial-box-switcher/script.js
@@ -1,1 +1,7 @@
-const token = document.querySelector('token');
+const testimonialsContainer = document.querySelector('.testimonials-container');
+const testimonial = document.querySelector('.testimonial');
+const userImage = document.querySelector('.user-image');
+const userName = document.querySelector('.username');
+const role = document.querySelector('.role');
+
+const testimonals
