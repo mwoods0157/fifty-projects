3f4520685fed606f337b06fd7fa0bd6a54a83f1e
Matthew <matthew.woods0157@gmail.com>
2022-08-31 13:44:53 -0400

    Small change

--- day17-movie-app/script.js
+++ day17-movie-app/script.js
@@ -26,7 +26,6 @@ function showMovies(movies) {
         movieEl.classList.add('movie');
 
         movieEl.innerHTML = `
-        <div class="movie">
             <img src="${IMG_PATH + poster_path}" alt="${title}">
             <div class="movie-info">
                 <h3>${title}</h3>
@@ -36,7 +35,6 @@ function showMovies(movies) {
                 <h3>Overview</h3>
                 ${overview}
             </div>
-        </div>
         `;
         main.appendChild(movieEl);
     })
