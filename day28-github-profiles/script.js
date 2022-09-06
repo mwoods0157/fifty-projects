62dcee38886be4d74fc60d3778e3cecaa0922dc0
Matthew <matthew.woods0157@gmail.com>
2022-09-06 11:38:54 -0400

    Event listener added to form

--- day28-github-profiles/script.js
+++ day28-github-profiles/script.js
@@ -1,10 +1,25 @@
 const APIURL = 'https://api.github.com/users/';
 
-getUser('bradtraversy');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
 
-function getUser(username) {
-    axios(APIURL + username)
-    .then(res => console.log(res.data))
-    .catch(err => console.log(err));
+async function getUser(username) {
+    try {
+        const { data } = await axios(APIURL + username);
+        console.log(data);
+    } catch(err) {
+        console.log(err);
+    }
 }
 
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const user = search.value;
+
+    if(user) {
+        getUser(user);
+
+        search.value = '';
+    }
+})
