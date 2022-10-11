86f48ae3d7c267b8359fe552c3223cd18fdc664f
Matthew <matthew.woods0157@gmail.com>
2022-10-11 17:29:04 -0400

    Building questions objecct

---
+++ day55-buzzfeed-clone/script.js
@@ -0,0 +1,23 @@
+const questionDisplay = document.getElementById('questions');
+const answerDisplay = document.getElementById('answer');
+
+const questions = [
+    {
+        id: 0,
+        text: "Pick a vacation destination:",
+        answers: [
+            {
+                text: "New York",
+                image: "",
+                alt: "Photo of the empire state building during daytime",
+                credit: "Oliver Niblett"
+            }
+        ]
+    },
+    {
+
+    },
+    {
+
+    }
+]
