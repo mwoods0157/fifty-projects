531861376d4c69cf27e3c1de65ddc1c167ab417a
Matthew <matthew.woods0157@gmail.com>
2022-11-07 17:02:03 -0500

    checkWin function added

--- day57-tic-tac-toe/script.js
+++ day57-tic-tac-toe/script.js
@@ -1,17 +1,36 @@
 const X_CLASS = 'x';
 const CIRCLE_CLASS = 'circle';
+const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
 const cellElements = document.querySelectorAll('[data-cell]');
 const board = document.getElementById('board');
 let circleTurn;
 
-cellElements.forEach(cell => {
-    cell.addEventListener('click', handleClick, { once: true })
-})
+startGame();
+
+function startGame() {
+    circleTurn = false;
+    cellElements.forEach(cell => {
+        cell.addEventListener('click', handleClick, { once: true })
+    });
+    setBoardHoverClass();
+}
 
 function handleClick(e) {
     const cell = e.target;
     const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
     placeMark(cell, currentClass);
+    if (checkWin(currentClass)) {
+        console.log('Winner');
+    }
     //check win
     //check draw
     //switch turns
@@ -35,4 +54,12 @@ function setBoardHoverClass() {
     } else {
         board.classList.add(X_CLASS);
     }
+}
+
+function checkWin(currentClass) {
+    return WINNING_COMBINATIONS.some(combinations => {
+        return combinations.every(index => {
+            return cellElements[index].classList.contains(currentClass)
+        })
+    })
 }
